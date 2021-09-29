02f836006a1acd3290313c95f864a2345dc9d710
Roxana Petcu <roxana_petcu26@yahoo.com>
2021-09-29 16:21:36 +0200

    labels based on tokenized length

--- experiments/nlg/cluster_data.py
+++ experiments/nlg/cluster_data.py
@@ -1,69 +1,39 @@
-# imports
-import os
 import argparse
-import math
 import torch
-import pandas as pd
 import numpy as np
-from torch.utils.data import (DataLoader, SequentialSampler, TensorDataset)
-from utils_nlg import processors, InputExample
-
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-import collections
+from transformers import AutoTokenizer
+from utils_nlg import InputExample
+from evaluate_nlg import answer_index, model_index, load_examples
 
-import time
 
 class InputConcat(InputExample):
+    """Contains the concatenation of the context and its correct answer, the length of the result and the label of the datapoint"""    
+
     def __init__(self, input_obj, context_concat, length = 0, label = 0):
+
         InputExample.__init__(self, input_obj.example_id, input_obj.context, input_obj.endings, input_obj.label)
         self.context_concat = context_concat
         self.length = length
         self.label = label
 
-# index for indices to answers
-answer_index = {
-    0: "A",
-    1: "B",
-    2: "C",
-    3: "D",
-}
-
-def load_examples(args):
-    """
-    Function for loading the examples.
-    Inputs:
-        args - Namespace object from the argument parser
-    Outputs:
-        examples - List of development set examples
-    """
-
-    # Load data features
-    print("Creating features from dataset file at {}".format(args.data_dir))
-    processor = processors["mutual"]()
-    label_list = processor.get_labels()
-    examples = processor.get_dev_examples(args.data_dir)
-    print("Number of evaluation instances: {}".format(len(examples)))
-
-    # Return the items
-    return examples
-
 def concat_data(eval_dataset):
+    """This method concatenates the data: each dialogue with its correct continuation
+
+    Args:
+        eval_dataset (list): Evaluation dataset
+
+    Returns:
+        list: Concatenated evaluation dataset
+    """    
 
     label_options = list(answer_index.values())
     eval_dataset_concat = []
 
     for example in eval_dataset:
-
-        # print("Context: ", example.context)
-        # print("Possible answers: ", example.endings)
-        # print("Example ID: ", example.example_id)
-        # print("True label: ", example.label)
-        # print("True label: ", label_options.index(example.label))
-        # print(example.endings[label_options.index(example.label)])
-        # print(example.context + " " + example.endings[label_options.index(example.label)], '\n')
 
         context_concat = example.context + " " + example.endings[label_options.index(example.label)]
         new_example = InputConcat(example, context_concat)
@@ -71,45 +41,91 @@ def concat_data(eval_dataset):
 
     return eval_dataset_concat
 
+def plot_histogram(input, xlabel):
+    """Plot a histogram given data
+
+    Args:
+        input (list): Input data that is plotted
+        xlabel ([type]): Label for the OX axis
+    """
+
+    plt.rcParams.update({'figure.figsize':(7,5), 'figure.dpi':100})
+
+    plt.hist(input, bins=50)
+    plt.gca().set(title='Frequency Histogram', xlabel=xlabel, ylabel='Frequency')
+    plt.show()
+
+def assign_length_labels(eval_dataset_concat, length):
+    """This method assigns a label to each datapoint given its length and the data distribution
+
+    Args:
+        eval_dataset_concat (list): Concatenated evaluation dataset
+        length (list): List of all lengths for faster computation of quartiles
+    """    
+    
+    q_1, q_2, q_3 = np.quantile(length, 0.25), np.quantile(length, 0.50), np.quantile(length, 0.75)
+
+    for example in eval_dataset_concat:
+        example.label = 0 if example.length <= q_1 else 1 if example.length <= q_2 else 2 if example.length <= q_3 else 3
+    
 def get_length(eval_dataset_concat):
+    """Calculate the length of each datapoint
+
+    Args:
+        eval_dataset_concat (list): Concatenates evaluation dataset
+    """    
 
     length = []
+
     for example in eval_dataset_concat:
         example.length = len(example.context_concat)   
         length.append(example.length)
     
-    start = time.time()
-    q_1, q_2, q_3 = np.quantile(length, 0.25), np.quantile(length, 0.50), np.quantile(length, 0.75)
+    assign_length_labels(eval_dataset_concat, length)
+    plot_histogram(length, xlabel='Length of Dialogue in characters')
 
+def get_token_length(eval_dataset_concat):
+    """Compute average token length given tokenization processes for all models
+
+    Args:
+        eval_dataset_concat (list): Concatenated evaluation dataset
+    """    
+    
+    length_tokenizers = np.empty((len(model_index), len(eval_dataset_concat)))
+    index_m = 0
+
+    for model in model_index.values():
+        
+        print("Current model: ", model)
+
+        tokenizer = AutoTokenizer.from_pretrained(model)
+        tokenizer.pad_token = "[PAD]"
+  
+        length_tokenizers[index_m] = [len(tokenizer.tokenize(eval_dataset_concat[i].context_concat)) for i in range(len(eval_dataset_concat))]    
+        index_m += 1
+    
+    mean_length_tokenizers = np.mean(length_tokenizers, axis = 0)
+    
+    j = 0
     for example in eval_dataset_concat:
-        if example.length <= q_1:
-            example.label = 0
-        elif example.length <= q_2:
-            example.label = 1
-        elif example.length <= q_3:
-            example.label = 2
-        else:
-            example.label = 3
-
-    print(time.time()-start)
-    # Plot Histogram on length
-    plt.rcParams.update({'figure.figsize':(7,5), 'figure.dpi':100})
-
-    plt.hist(length, bins=50)
-    plt.gca().set(title='Frequency Histogram', xlabel='Length of Dialogue in characters', ylabel='Frequency')
-    plt.show()
+        example.length = mean_length_tokenizers[j] 
+        j += 1
+        
+    assign_length_labels(eval_dataset_concat, mean_length_tokenizers)
+    plot_histogram(mean_length_tokenizers, xlabel='Length of Tokenized Dialogue')
     
 def get_tfidf(eval_dataset_concat):
-    
+    """Compute tf-idf scores for each datapoint and form clusters by applying KMeans on the data
+
+    Args:
+        eval_dataset_concat (list): Concatenated evaluation dataset
+    """    
     corpus = [example.context_concat for example in eval_dataset_concat]
     
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(corpus)
     
-    print("WORDS: ", vectorizer.get_feature_names_out())   
-    
     clusters = KMeans(n_clusters=4, n_init=128).fit_predict(X)
-    print("LABELS: ", collections.Counter(clusters))
     
     for i, example in enumerate(eval_dataset_concat):
         example.label = clusters[i]
@@ -128,67 +144,52 @@ def get_tfidf(eval_dataset_concat):
 
     plt.show()
 
-def data_partition(args, device, partition):
-    """
-    Function for evaluating the model.
-    Inputs:
-        args - Namespace object from the argument parser
-        device - PyTorch device to place the model on
-    Outputs:
-        ?
-    """
-    
-    print('Loading data..')
-    eval_dataset = load_examples(args)
+def data_partition(args,  eval_dataset):
+    """This method selects the desired data partition
+
+    Args:
+        args (Namespace): Object from the argument parser
+        eval_dataset (list): Evaluation dataset containing dialogue and all 4 possible answers
+    """    
+
+    # First concatenate the data
     eval_dataset_concat = concat_data(eval_dataset)
     
-    print("here: ", eval_dataset_concat[885].example_id)
-    print("here: ", eval_dataset_concat[885].context_concat, '\n')
-    
-    if partition == 'length':
+    if args.partition == 'length':
         get_length(eval_dataset_concat)
-    elif partition == 'tf-idf':
+    elif args.partition == 'tf-idf':
         get_tfidf(eval_dataset_concat)
-    
-def main(args):
-    """
-    Function for handling the arguments and starting data clustering
-    Inputs:
-        args - Namespace object from the argument parser
-    """
+    elif args.partition == 'token_length':
+        get_token_length(eval_dataset_concat)
 
-    # Check if GPU is available
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def main(args):
+    """Handles the arguments and starts data partitioning
+
+    Args:
+        args (Namespace): Object from the argument parser
+    """    
 
     # Print the given parameters
-    print('-----EVALUATION PARAMETERS-----')
-    print('Device: {}'.format(device))
-    print('Max sequence length: {}'.format(args.max_seq_length))
+    print('-----PARAMETERS-----')
     print('Data directory: {}'.format(args.data_dir))
-    print('Output directory: {}'.format(args.output_dir))
+    print('Data partitioning on: {}'.format(args.partition))
     print('-----------------------------')
 
-    # Start evaluation
-
-    data_partition(args, device, partition = 'tf-idf')
+    # Load data and start data partitioning
+    eval_dataset = load_examples(args)
+    data_partition(args, eval_dataset)
 
 
 # Command line arguments parsing
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     # Hyperparameters
-    parser.add_argument('--model', default='gpt2', type=str,
-                        help='Generator model type to use. Default is gpt2.',
-                        choices=['gpt2'])
     parser.add_argument("--data_dir", default='data/mutual', type=str,
                         help="The input data dir. Should contain the .tsv files (or other data files) for the task. Default is data/mutual.")
-    parser.add_argument("--output_dir", default='experiment_outputs/', type=str,
-                        help="The output directory for the .csv files. Default is experiment_outputs/.")
-    parser.add_argument("--max_seq_length", default=128, type=int,
-                        help="The maximum total input sequence length after tokenization. Sequences longer "
-                             "than this will be truncated, sequences shorter will be padded. Default is 128.")
+    parser.add_argument("--partition", default='length', type=str,
+                        help="Types of data partition. Can be one of the following: [length, token_length, tf-idf]/.")
 
     # Parse the arguments
     args = parser.parse_args()
